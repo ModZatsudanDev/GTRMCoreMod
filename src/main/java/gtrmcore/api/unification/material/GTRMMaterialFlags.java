b72f4880b54357d4cf92be7e40c64b67d2ae2cbd
tier940 <tsubasa.ito@relightings.com>
2024-01-26 22:24:08 +0900

    Wood tools

--- src/main/java/gtrmcore/api/unification/material/GTRMMaterialFlags.java
+++ src/main/java/gtrmcore/api/unification/material/GTRMMaterialFlags.java
@@ -1,9 +1,12 @@
 package gtrmcore.api.unification.material;
 
+import gregtech.api.unification.material.Materials;
+import gregtech.api.unification.material.properties.PropertyKey;
+import gregtech.api.unification.material.properties.ToolProperty;
+
 public class GTRMMaterialFlags {
 
     public static void init() {
-        // Materials.Wood.setProperty(PropertyKey.TOOL,
-        // new ToolProperty(10.0F, 2.0F, 10, 1));
+        Materials.Wood.setProperty(PropertyKey.TOOL, new ToolProperty(10.0F, 2.0F, 10, 1));
     }
 }
