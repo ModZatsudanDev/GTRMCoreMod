e50411726b5586ee3fa1a400be162116a9979501
MaimoCh <aoishima001@gmail.com>
2024-02-29 17:24:52 +0900

    hag versionとの調節③

--- src/main/java/gtrmcore/api/unification/material/GTRMMarkerMaterials.java
+++ src/main/java/gtrmcore/api/unification/material/GTRMMarkerMaterials.java
@@ -7,8 +7,8 @@ import gtrmcore.api.GTRMValues;
 public class GTRMMarkerMaterials {
 
     public static class Component {
-        public static final Material LOW_STEAM = MarkerMaterial.create("low_steam");
-        public static final Material HIGH_STEAM = MarkerMaterial.create("high_steam");
+        public static final Material LOW = MarkerMaterial.create("low");
+        public static final Material HIGH = MarkerMaterial.create("high");
 
     }
 }
--- src/main/java/gtrmcore/common/items/GTRMMetaItem1.java
+++ src/main/java/gtrmcore/common/items/GTRMMetaItem1.java
@@ -30,59 +30,58 @@ public class GTRMMetaItem1 extends StandardMetaItem {
         // Circuits 1001~1100
 
         // steam valves
-        STEAM_VALVE = addItem(1001, "valve.steam").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.LOW_STEAM);
-        ELECTRIC_STEAM_VALVE = addItem(1002,"valve.electric").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.LOW_STEAM);
+        STEAM_VALVE = addItem(1001, "valve.steam").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.LOW);
+        ELECTRIC_STEAM_VALVE = addItem(1002,"valve.electric").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.LOW);
         // high steam valves
-        HIGH_STEAM_VALVE = addItem(1003, "valve.high_steam").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.HIGH_STEAM);
-        ELECTRIC_HIGH_STEAM_VALVE = addItem(1004,"valve.high_electric").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.HIGH_STEAM);
+        HIGH_STEAM_VALVE = addItem(1003, "valve.high_steam").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.HIGH);
+        ELECTRIC_HIGH_STEAM_VALVE = addItem(1004,"valve.high_electric").setUnificationData(GTRMOrePrefix.valve, GTRMMarkerMaterials.Component.HIGH);
 
         // Components 1101~1200
+        // low steam
+        ELECTRIC_MOTOR_LOW = addItem(1101, "electric.motor.low");
+        ELECTRIC_PUMP_LOW = addItem(1102, "electric.pump.low");
+        CONVEYOR_MODULE_LOW = addItem(1103, "conveyor.module.low");
+        ELECTRIC_PISTON_LOW = addItem(1104, "electric.piston.low");
+        ROBOT_ARM_LOW = addItem(1105, "robot.arm.low");
+        FIELD_GENERATOR_LOW = addItem(1106, "field.generator.low");
+        EMITTER_LOW = addItem(1107, "emitter.low");
+        SENSOR_LOW = addItem(1108, "sensor.low");
+
+        //high steam
+        ELECTRIC_MOTOR_HIGH = addItem(1109, "electric.motor.high");
+        ELECTRIC_PUMP_HIGH = addItem(1110, "electric.pump.high");
+        CONVEYOR_MODULE_HIGH = addItem(1111, "conveyor.module.high");
+        ELECTRIC_PISTON_HIGH = addItem(1112, "electric.piston.high");
+        ROBOT_ARM_HIGH = addItem(1113, "robot.arm.high");
+        FIELD_GENERATOR_HIGH = addItem(1114, "field.generator.high");
+        EMITTER_HIGH = addItem(1115, "emitter.high");
+        SENSOR_HIGH = addItem(1116, "sensor.high");
 
         // ULV
-        ELECTRIC_MOTOR_ULV = addItem(1101, "electric.motor.ulv");
-        ELECTRIC_PUMP_ULV = addItem(1102, "electric.pump.ulv")
+        ELECTRIC_MOTOR_ULV = addItem(1117, "electric.motor.ulv");
+        ELECTRIC_PUMP_ULV = addItem(1118, "electric.pump.ulv")
                 .addComponents(new TooltipBehavior(lines -> {
                     lines.add(I18n.format("metaitem.electric.pump.tooltip"));
                     lines.add(I18n.format("gregtech.universal.tooltip.fluid_transfer_rate", 1280 / 4));
                 }));
-        CONVEYOR_MODULE_ULV = addItem(1103, "conveyor.module.ulv")
+        CONVEYOR_MODULE_ULV = addItem(1119, "conveyor.module.ulv")
                 .addComponents(new TooltipBehavior(lines -> {
                     lines.add(I18n.format("metaitem.conveyor.module.tooltip"));
                     lines.add(I18n.format("gregtech.universal.tooltip.item_transfer_rate", 2));
                 }));
-        ELECTRIC_PISTON_ULV = addItem(1104, "electric.piston.ulv");
-        ROBOT_ARM_ULV = addItem(1105, "robot.arm.ulv")
+        ELECTRIC_PISTON_ULV = addItem(1120, "electric.piston.ulv");
+        ROBOT_ARM_ULV = addItem(1121, "robot.arm.ulv")
                 .addComponents(new TooltipBehavior(lines -> {
                     lines.add(I18n.format("metaitem.robot.arm.tooltip"));
                     lines.add(I18n.format("gregtech.universal.tooltip.item_transfer_rate", 2));
                 }));
-        FLUID_REGULATOR_ULV = addItem(1106, "fluid.regulator.ulv")
+        FLUID_REGULATOR_ULV = addItem(1122, "fluid.regulator.ulv")
                 .addComponents(new TooltipBehavior(lines -> {
                     lines.add(I18n.format("metaitem.fluid.regulator.tooltip"));
                     lines.add(I18n.format("gregtech.universal.tooltip.fluid_transfer_rate", 320 / 4));
                 }));
-        FIELD_GENERATOR_ULV = addItem(1107, "field.generator.ulv");
-        EMITTER_ULV = addItem(1108, "emitter.ulv");
-        SENSOR_ULV = addItem(1109, "sensor.ulv");
-
-        // low steam
-        ELECTRIC_MOTOR_LOW = addItem(1110, "electric.motor.low");
-        ELECTRIC_PUMP_LOW = addItem(1111, "electric.pump.low");
-        CONVEYOR_MODULE_LOW = addItem(1112, "conveyor.module.low");
-        ELECTRIC_PISTON_LOW = addItem(1113, "electric.piston.low");
-        ROBOT_ARM_LOW = addItem(1114, "robot.arm.low");
-        FIELD_GENERATOR_LOW = addItem(1115, "field.generator.low");
-        EMITTER_LOW = addItem(1116, "emitter.low");
-        SENSOR_LOW = addItem(1117, "sensor.low");
-
-        //high steam
-        ELECTRIC_MOTOR_HIGH = addItem(1118, "electric.motor.high");
-        ELECTRIC_PUMP_HIGH = addItem(1119, "electric.pump.high");
-        CONVEYOR_MODULE_HIGH = addItem(1120, "conveyor.module.high");
-        ELECTRIC_PISTON_HIGH = addItem(1121, "electric.piston.high");
-        ROBOT_ARM_HIGH = addItem(1122, "robot.arm.high");
-        FIELD_GENERATOR_HIGH = addItem(1123, "field.generator.high");
-        EMITTER_HIGH = addItem(1124, "emitter.high");
-        SENSOR_HIGH = addItem(1125, "sensor.high");
+        FIELD_GENERATOR_ULV = addItem(1123, "field.generator.ulv");
+        EMITTER_ULV = addItem(1124, "emitter.ulv");
+        SENSOR_ULV = addItem(1125, "sensor.ulv");
     }
 }
